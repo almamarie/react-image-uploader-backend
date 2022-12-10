e57f70429f7634da367870d658b26c0e08d0dd19
almamarie <almamariea97@gmail.com>
2022-12-10 13:06:11 +0000

    Updated the backend to connect to the postgres database on render.com

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import quote_plus
 from settings import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
 
 
@@ -8,8 +9,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
-    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+    DB_USER, DB_PASSWORD, quote_plus(DB_HOST), DB_NAME)
+# DB_PATH = "postgres://marieloumar:c89V948PzuNvrh8nb6nLPdJfE8n9YuO5@dpg-cea7mr6n6mphc8sj172g-a.oregon-postgres.render.com/fileupload"
+# DB_PATH = "postgres://marieloumar:c89V948PzuNvrh8nb6nLPdJfE8n9YuO5@dpg-cea7mr6n6mphc8sj172g-a/fileupload"
 
+# postgres://marieloumar:c89V948PzuNvrh8nb6nLPdJfE8n9YuO5@dpg-cea7mr6n6mphc8sj172g-a.oregon-postgres.render.com/fileupload
 
 # Enable debug mode.
 DEBUG = True
